b11d0c0eb7c2ee36d158a853ad6964a24124b4f0
KBigForest <gksfka12@naver.com>
2023-01-12 15:12:52 +0900

    웹페이지 만들고 적용하는 과정

--- blog/models.py
+++ blog/models.py
@@ -8,4 +8,6 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} (수정일 : {self.updated_at})'
+    def get_absolute_url(self):
+        return f'/blog/{self.pk}/'
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def index(requset): 
+    posts = Post.objects.order_by('-pk')
+    return render(requset, 'blog/index.html',{'posts':posts,})
+
+def single_post_page(request, value):
+    post = Post.objects.get(pk=value)
+    
+    return render(request, 'blog/single_post_page.html',
+                  {'post':post,})
